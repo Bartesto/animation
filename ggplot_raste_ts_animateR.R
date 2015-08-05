305d7950a425a37b75e837a3c31a2a8994b9e4f8
Bartesto <barthuntley@iinet.net.au>
2015-08-05 15:22:43 +0800

    updated remove temp files

--- ggplot_raste_ts_animateR.R
+++ ggplot_raste_ts_animateR.R
@@ -39,9 +39,15 @@ library(plyr)
 # Required directory paths
 imdir <- "W:\\usgs\\110078"#path/row folder in USGS
 anidir <- "Z:\\DOCUMENTATION\\BART\\R\\R_DEV\\animation"#working dir: location of shape files
+tmpdir <- "C:\\Users\\barth\\AppData\\Local\\Temp\\R_raster_barth"
+pathrow <- substr(imdir, 9, 14)
+
+setwd(anidir)
+folder <- paste0(pathrow, "_ani_tifs_", Sys.Date())
+if(!file.exists(folder)){ dir.create(folder)} # create another folder
+tifdir <- paste0(anidir, "\\", folder)
 
 # Read in shape files
-setwd(anidir)
 ext <- extent(readOGR(dsn = "Z:\\DOCUMENTATION\\BART\\R\\R_DEV\\animation", 
                       "aoi_LG_extent_polygons"))#get extent only of aoi
 # Special workflow to fortify for use in ggplot
@@ -50,7 +56,7 @@ enclosure@data$id <- rownames(enclosure@data)
 enclosure.points <- fortify(enclosure, region = "id")
 enclosure.df <- join(enclosure.points, enclosure@data, by = "id")
 
-site <- readOGR(dsn = ".", layer = "LG_monitor_sites_example")
+site <- readOGR(dsn = ".", layer = "LG_monitor_sites_example_sml")
 site@data$id <- rownames(site@data)
 site.points <- fortify(site, region = "id")
 site.df <- join(site.points, site@data, by = "id")
@@ -61,7 +67,7 @@ d[,2] <- as.Date(d[,2], "%Y-%m-%d")
 d$dnum <- as.numeric(d[,2])
 df <- d[-1]
 
-# Code to trim to match animation equence (remove Display folder and bad L7's)
+# Code to trim to match animation sequence (remove Display folder and bad L7's)
 setwd("W:\\usgs\\110078")#imdir
 allfiles <- list.files(recursive = TRUE)
 result <- allfiles[grepl("*pre.ers", allfiles)]
@@ -73,10 +79,81 @@ sensor <- substr(result, 10, 11)#get just sensor
 ind.df <- data.frame(s = sensor, d = imdate, stringsAsFactors = FALSE)
 #create index to and remove l7 scan error images
 nL7.index <- (ind.df$s == "l7") & (ind.df$d > as.Date("2003-05-30"))
-df2 <- df[which(!nL7.index), ]#use which!
+sites.df <- df[which(!nL7.index), ]#use which!
 folds.no.7 <- fold[!nL7.index]#this used in images - CHECK lengths same
 
+############################################################################
+# Manipulate raster data for image plot
 
+i=102
+for (i in 1: length(folds.no.7)){
+        setwd(paste0(imdir, "\\", folds.no.7[i]))
+        imname <- list.files(pattern = "pre.ers")
+        gtname <- paste0("L_", as.Date(substr(imname, 12,17), "%d%m%y"), 
+                         "_", pathrow, ".tif")
+        rdate <- format(as.Date(substr(imname, 12,17), "%d%m%y"), "%b %Y")
+        rawlt <- stack(imname)
+       
+        ngn <- crop(x=rawlt, ext)
+        setwd(tifdir)
+        writeRaster(x=ngn, filename=gtname, format="GTiff", overwrite=T)
+        t.name <- list.files(pattern = ".tif")
+        mb_ras <- stack(t.name)
+        mb_ras <- stretch(x=mb_ras, minv=0, maxv=255)
+        mb_df <- raster::as.data.frame(mb_ras, xy=T)
+        mb_df <- data.frame(x=mb_df[,1], y=mb_df[,2], b1=mb_df[,3], b2=mb_df[,4], 
+                            b3=mb_df[,5], b4=mb_df[,6], b5=mb_df[,7],
+                            b6=mb_df[,8])
+        mb_df <- mb_df[complete.cases(mb_df),]#remove NA's
+        #create raster ggplot object
+        map <- ggplot() +
+                coord_equal() + theme_bw() + 
+                geom_tile(data=mb_df, aes(x=x, y=y, fill=rgb(b5,b4,b3, 
+                                                             maxColorValue = 255))) + 
+                scale_fill_identity() +
+                scale_x_continuous(breaks=c(304000, 350000),
+                                   labels=c(304000, 350000), expand = c(0,0)) +
+                scale_y_continuous(breaks=c(7090000, 7130000), 
+                                   labels=c(7090000, 7130000), expand = c(0,0)) +
+                theme(panel.grid=element_blank(), plot.title = element_text(size = 10))+
+                xlab("")+ ylab("")+
+                labs(title=rdate)
+        p1 <- map + geom_path(data=enclosure.df, aes(x=long,y=lat,group=group), 
+                              colour="yellow", size=1)+
+                geom_path(data=site.df, aes(x=long,y=lat,group=group), 
+                          colour="red", size=2)
+        
+        #create ts ggplot object ####CHANGES!!!!!!!!!
+        x <- sites.df[, 4]
+        y <- sites.df[, 3]
+        dfall <- data.frame(x=x, y=200-y)##Flipped i35
+        dfsub <- dfall[i, ]
+        p2 <- ggplot()+
+                #geom_point(data = dfall, aes(x=x, y=y))+
+                geom_point(data = dfsub, aes(x=x, y=y), colour = "red", shape = 10, size =5)+
+                geom_line(data = dfall, aes(x=x, y=y))+
+                geom_vline(xintercept = dfsub[,1], colour = "red")+
+                theme_bw()+
+                xlab("")+
+                ylab("index")+
+                
+                coord_cartesian(xlim = c(6000, 16700),
+                                ylim = c(0, 200))+
+                theme(axis.ticks = element_blank(),
+                      axis.text.x = element_blank(),
+                      legend.position = "none")
+        png(filename = "testY.png", width = 1000, height = 1000)#, width = 842, height = 250
+        grid.newpage() # Open a new page on grid device
+        pushViewport(viewport(layout = grid.layout(7, 5)))
+        print(p1, vp = viewport(layout.pos.row = 1:5, layout.pos.col = 1:5))
+        print(p2, vp = viewport(layout.pos.row = 6:7, layout.pos.col = 2:4)) 
+        dev.off()
+        
+        ##Clean up "C:\\Users\\barth\\AppData\\Local\\Temp\\R_raster_barth"
+        tmp.list <- list.files(path = tmpdir, full.names = TRUE)
+        file.remove(tmp.list)#Dangerous be careful
+        
+}
 
 
 
